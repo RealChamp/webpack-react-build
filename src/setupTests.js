923eb38973179685f160c4f81c3d5326a8dfc8aa
= <matvey.w2015@mail.ru>
2020-10-11 20:03:59 +0400

    modify jest config and setup jest config

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,14 @@
-import {configure} from 'enzyme'
+import {configure, shallow, render, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
 
 configure({adapter: new Adapter})
+
+global.shallow = shallow
+global.render = render
+global.mount = mount
+global.toJson = toJson
+
+console.error = message => {
+    throw new Error(message)
+}
