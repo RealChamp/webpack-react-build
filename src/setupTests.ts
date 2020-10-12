7a0aab3d1574d6d6f2ebcba332bca6f7ec241def
= <matvey.w2015@mail.ru>
2020-10-12 16:53:48 +0400

    rename setupTests file & add types for enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,14 +1,15 @@
+const globalAny:any = global;
 import {configure, shallow, render, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 
 configure({adapter: new Adapter})
 
-global.shallow = shallow
-global.render = render
-global.mount = mount
-global.toJson = toJson
+globalAny.shallow = shallow
+globalAny.render = render
+globalAny.mount = mount
+globalAny.toJson = toJson
 
-console.error = message => {
+console.error = (message: string) => {
     throw new Error(message)
 }
